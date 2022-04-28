6ce0f50da57c738be4de4ccdc19d8d2d70c2e48f
Mike Arthur <58674918+Onlyfortheweak@users.noreply.github.com>
2022-04-27 22:06:29 -0400

    Add files via upload

---
+++ account.java
@@ -0,0 +1,43 @@
+
+public class account {
+	
+	private String username;
+	private String password;
+	private String[] statCats = {"Pawns", "Bishops", "Rooks", "Knights", "Queens", "Wins"};
+	private int[] stats;
+	
+	public account(String un, String pw) {
+		this.username = un;
+		this.password = pw;
+	}
+	
+	public account(String u, String pw, int p, int b, int r, int k, int q, int w) {
+		this.username = u;
+		this.password = pw;
+		stats[0] = p;
+		stats[1] = b;
+		stats[2] = r;
+		stats[3] = k;
+		stats[4] = q;
+		stats[5] = w;
+		
+	}
+	
+	public String getPassword() {
+		return this.password;
+	}
+	
+	public void changePassword(String old, String newPass) {
+		if(old.equals(this.password)) this.password = newPass;
+	}
+	
+	public String getUsername() {
+		return this.username;
+	}
+	
+    public void updateStats(String s) {
+    	for(int i = 0; i < statCats.length; i++) {
+    		if(statCats[i].equals(s)) stats[i]++;
+    	}
+    }
+}
